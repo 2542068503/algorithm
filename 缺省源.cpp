fcf6ef4abbed8ea02044b0a0203d43c53cff1aeb
zhangshiyan <92670153+2542068503@users.noreply.github.com>
2026-08-08 15:16:50 +0800

    Add initial C++ source file with main function

---
+++ 缺省源.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const ll INF = 1e18;
+mt19937 rnd((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
+#define pb push_back
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	return 0;
+}
