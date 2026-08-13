7ec32631c4acdf3917dfc33b0d61c5d59e63856b
zhangshiyan <92670153+2542068503@users.noreply.github.com>
2026-08-13 19:51:12 +0800

    Refactor exgcd function parameter passing
    Removed redundant comment and clarified parameter order in exgcd function.

--- exgcd(扩展欧几里得).cpp
+++ exgcd(扩展欧几里得).cpp
@@ -5,9 +5,7 @@ ll exgcd(ll a, ll b, ll &x, ll &y)
 		x = 1, y = 0;
 		return a;
 	}
-	ll d = exgcd(b, a % b, y, x);
+	ll d = exgcd(b, a % b, y, x); // 注意是先传 y 再传 x  
 	y -= (a / b) * x;
 	return d;
 }
-
-// 注意是先传 y 再传 x  
