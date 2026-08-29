5d544f73c671df793a5d6fd98af864fa34444c51
zhangshiyan <92670153+2542068503@users.noreply.github.com>
2026-08-29 19:35:51 +0800

    Implement Tarjan's algorithm for biconnected components

---
+++ vcc(点双连通分量).cpp
@@ -0,0 +1,42 @@
+void tarjan(ll u, ll f)
+{
+	dfn[u] = low[u] = ++tdfn;
+	stk.push(u);
+	ll child = 0;
+	for (ll v : g[u])
+	{
+		if (v == f)
+		{
+			continue;
+		}
+		if (dfn[v])
+		{
+			low[u] = min(low[u], dfn[v]);
+		}
+		else
+		{
+			child++;
+			tarjan(v, u);
+			low[u] = min(low[u], low[v]);
+			if (low[v] >= dfn[u])
+			{
+				bcc_cnt++;
+				bcc_id[bcc_cnt].pb(u);
+				while (!stk.empty())
+				{
+					ll cur = stk.top();
+					stk.pop();
+					bcc_id[bcc_cnt].pb(cur);
+					if (cur == v)
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+	if (f == 0 && child == 0)
+	{
+		bcc_id[++bcc_cnt].pb(u);
+	}
+}
